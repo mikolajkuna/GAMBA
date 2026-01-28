1098e1b281a8dc231cd2db3cf61af993a0afd904
mikolajkuna <mikolaj.kuna@gmail.com>
2026-01-28 10:39:59 +0100

    Update train.py

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,21 +1,30 @@
-"""
-Code to train models
-"""
+# src/modeling/train.py
 
-from pathlib import Path
-
-from src.config import MODELS_DIR, PROCESSED_DATA_DIR
+from pygam import LinearGAM, s, f, te
+from sklearn.utils import compute_sample_weight
 
 
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    pass
+def build_gamba():
+    return LinearGAM(
+        s(0, constraints="monotonic_inc") +
+        f(1) +
+        s(2, constraints="monotonic_inc") +
+        s(3, constraints="monotonic_inc") +
+        s(4, constraints="monotonic_inc") +
+        f(5) +
+        s(6, constraints="monotonic_dec") +
+        s(7) +
+        te(1, 3, lam=10)
+    )
 
 
-if __name__ == "__main__":
-    main()
+def train_gamba(X, y):
+    weights = compute_sample_weight(
+        class_weight="balanced",
+        y=X[:, 1]
+    )
+
+    model = build_gamba()
+    model.fit(X, y, weights=weights)
+
+    return model, weights
